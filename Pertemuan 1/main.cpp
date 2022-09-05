a5b55ffb357ee4bf105533c10dfaf215a58385d3
selviij <112878616+selviij@users.noreply.github.com>
2022-09-05 21:06:06 +0700

    Pertemuan 1

---
+++ Pertemuan 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Selvi Ijayanti" << endl;
+    cout << "A11.2022.14139" << endl;
+    cout << "Ds. Candisari RT01/RW05, Kec. Purwodadi, Kab. Grobogan" << endl;
+    cout << "SMK ASTA MITRA PURWODADI" << endl;
+    return 0;
+}
