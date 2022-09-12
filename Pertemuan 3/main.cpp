9a37fb9a0c0bf64c7753b7fc06a87d1b5eeec777
selviij <112878616+selviij@users.noreply.github.com>
2022-09-12 18:00:11 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    /*
+    printf ("Test");
+    int= integer
+    char= character*/
+    int u = 19;
+    float t = 155.0;
+    char b = 'M';
+    string ub = "xxl";
+    cout << "Umur saya: " << u;
+    cout << endl;
+    cout << "Tinggi badan: " << t;
+    cout << "\nUkuran baju: " << b;
+    cout << "\nUkuran baju: " << ub;
+    return 0;
+
+}
